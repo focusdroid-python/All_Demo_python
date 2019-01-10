d40a2fbca9765e1eb33242c1441cb92d6bab9aa6
focusdroid-python <focusdroid@126.com>
2019-01-10 22:37:32 +0800

    gevent遇到延时就执行

--- 201901/0110/06-gevent.py
+++ 201901/0110/06-gevent.py
@@ -1,7 +1,26 @@
 import gevent
 
-def test(n):
+def test1(n):
 	for i in range(n):
 		print(gevent.getcurrent(), i)
 
+def test2(n):
+	for i in range(n):
+		print(gevent.getcurrent(), i)
+
+def test3(n):
+	for i in range(n):
+		print(gevent.getcurrent(), i)
+
+print('----1----')
+g1 = gevent.spawn(test1, 5)
+print('----2----')
+g2 = gevent.spawn(test2, 5)
+print('----3----')
+g3 = gevent.spawn(test3, 5)
+print('----4----')
+
+g1.join()
+g2.join()
+g3.join()
 
